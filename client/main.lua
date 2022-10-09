90d45aeea1cd60cf70b545cc46313ac761e69d2b
DarkStarFury <rishikesh17@gmail.com>
2022-10-10 01:29:49 +0530

    Initial commit

---
+++ client/main.lua
@@ -0,0 +1,53 @@
+-- Events
+
+RegisterNetEvent('qb-printer:client:UseDocument', function(ItemData)
+    local DocumentUrl = ItemData.info.url ~= nil and ItemData.info.url or false
+    SendNUIMessage({
+        action = "open",
+        url = DocumentUrl
+    })
+    SetNuiFocus(true, false)
+end)
+
+RegisterNetEvent('qb-printer:client:SpawnPrinter', function(ItemData)
+    local playerPed = PlayerPedId()
+    local coords    = GetEntityCoords(playerPed)
+    local forward   = GetEntityForwardVector(playerPed)
+    local x, y, z   = table.unpack(coords + forward * 1.0)
+
+    local model = `prop_printer_01`
+    RequestModel(model)
+    while (not HasModelLoaded(model)) do
+        Wait(1)
+    end
+    obj = CreateObject(model, x, y, z, true, false, true)
+    PlaceObjectOnGroundProperly(obj)
+    SetModelAsNoLongerNeeded(model)
+    SetEntityAsMissionEntity(obj)
+end)
+
+-- NUI
+
+RegisterNUICallback('SaveDocument', function(data)
+    if data.url then
+        TriggerServerEvent('qb-printer:server:SaveDocument', data.url)
+    end
+end)
+
+RegisterNUICallback('CloseDocument', function()
+    SetNuiFocus(false, false)
+end)
+
+-- Command
+
+RegisterNetEvent('qb-printer:client:useprinter', function()
+    local ped = PlayerPedId()
+    local pos = GetEntityCoords(ped)
+    -- local PrinterObject = GetClosestObjectOfType(pos.x, pos.y, pos.z, 1.5, `prop_printer_01`, false, false, false)
+    --if PrinterObject ~= 0 then
+        SendNUIMessage({
+            action = "start"
+        })
+        SetNuiFocus(true, true)
+    --end
+end)
